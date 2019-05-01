6c50902d15855c4fb703b770e3fbfd843995cbd1
revwhyte <revwhyte@gmail.com>
2019-05-01 18:14:18 -0300

    increased questions database

--- info.js
+++ info.js
@@ -1,6 +1,21 @@
 var questions = [
-    "cachorro quente é sanduíche?",
-    "você usa fio dental antes ou depois de escovar os dentes?",
+    "Cachorro quente é sanduíche?",
+    "Você usa fio dental antes ou depois de escovar os dentes?",
+    "Tem algum restaurante que você gosta perto de onde mora?",
+    "Li um livro recentemente. Você já leu ele?",
+    "Se pudesse escolher alguém no mundo pra falar ao telefone, quem escolheria?",
+    "Qual seria seu nome de rockstar: seu primeiro pet, seu último carro",
+    "Qual o maior mico que você já pagou?",
+    "Que superpoder você queria ter, mas que teria um revés e você aceitaria?",
+    "Se pudesse ir em qualquer lugar no mundo, qual seria?",
+    /*"",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",*/
 ];
 
 function generateTheme() {
