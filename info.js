a599a4c466c7321f90ea8c8599e7354df66c16a9
Philippe Correia <revwhyte@gmail.com>
2019-05-08 19:00:24 -0300

    versão final do funcionamento
    commit com versão funcional do projeto

--- info.js
+++ info.js
@@ -1,3 +1,26 @@
-const questions = Array();
+var questions = [
+    "Cachorro quente é sanduíche?",
+    "Você usa fio dental antes ou depois de escovar os dentes?",
+    "Tem algum restaurante que você gosta perto de onde mora?",
+    "Li um livro recentemente. Você já leu ele?",
+    "Se pudesse escolher alguém no mundo pra falar ao telefone, quem escolheria?",
+    "Qual seria seu nome de rockstar: seu primeiro pet, seu último carro",
+    "Qual o maior mico que você já pagou?",
+    "Que superpoder você queria ter, mas que teria um revés e você aceitaria?",
+    "Se pudesse ir em qualquer lugar no mundo, qual seria?",
+    /*"",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",*/
+];
 
-questions = { 1: "cachorro quente é sanduíche?" };
+function generateTheme() {
+    var output = questions[Math.floor(Math.random() * questions.length)];
+
+    var outing = document.getElementById("here");
+    outing.innerHTML = output;
+}
