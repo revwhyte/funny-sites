347c3fc88f84ed8fd225a9a67736965f6bf937db
Philippe Correia <revwhyte@gmail.com>
2020-08-03 13:56:25 -0300

    idea for localStorage use

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// generates a string value with all data
 function parseOutput() {
     
 }
+
+// saves data into web storage
+$("#botao_salvar").on('click', function(){
+    let chave = $("#nome_campo").attr('id'),
+        valor = $("#campo_valor").val()
+
+    localStorage.setItem(chave, valor)
+})
